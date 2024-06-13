fbc090b370d6496ab2537b9af470eaa95357bfbb
EORrikaOrtiz <166356895+EORrikaOrtiz@users.noreply.github.com>
2024-06-12 21:46:32 -0600

    Add files via upload

---
+++ ConectFour.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConectFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mahbub, for this activity I will work alone, sincerely Erika Ortiz ID 456980");
+        }
+    }
+}
